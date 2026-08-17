0818cb07edfa6345903411623b84229b69abc8cd
bsw2884-svg <bsw2884-svg@users.noreply.github.com>
2026-08-16 21:46:50 -0400

    Initial JST Command Center

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+const VERSION='jst-command-v1';
+const SHELL=['/','/index.html','/manifest.webmanifest','/icons/jst-192.png','/icons/jst-512.png','/icons/jst-maskable-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(SHELL))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
+ if(event.request.mode==='navigate'){
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(VERSION).then(cache=>cache.put('/index.html',copy));return response}).catch(()=>caches.match('/index.html')));
+  return;
+ }
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(VERSION).then(cache=>cache.put(event.request,copy))}return response})));
+});
